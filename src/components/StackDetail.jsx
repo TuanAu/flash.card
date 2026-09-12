a54dc9f223e19d2d8f53172752b569fed0bf2f22
TuanAu <adgtuan@gmail.com>
2026-09-12 14:31:18 +1000

    add suspense for cards

--- src/components/StackDetail.jsx
+++ src/components/StackDetail.jsx
@@ -1,19 +1,35 @@
-import { useEffect, useState } from "react";
+import { use, useState, Suspense } from "react";
 import { getCards } from "../db/database";
 import EmptyCard from "./EmptyCard";
 import CardList from "./CardList";
+import Loader from "./Loader";
+
+function Cards({ stackId, getCardsPromise }) {
+  const cardsFromDb = use(getCardsPromise);
+  const [cards, setCards] = useState(cardsFromDb);
+
+  return (
+    <div>
+      {cards.length === 0 ? (
+        <EmptyCard stackId={stackId} setCards={setCards} />
+      ) : (
+        <CardList stackId={stackId} setCards={setCards} cards={cards} />
+      )}
+    </div>
+  );
+}
+
 export default function StackDetail({ stackId, goBack }) {
-  const [cards, setCards] = useState([]);
+  const [getCardsPromise] = useState(() => {
+    const getCardsPromise = getCards(stackId);
+    const delayByDefaultPromise = new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
 
-  useEffect(() => {
-    async function getCardsFromDB() {
-      const cards = await getCards(stackId);
-      setCards(cards);
-    }
-
-    getCardsFromDB();
-  }, []);
-
+    return Promise.all([getCardsPromise, delayByDefaultPromise]).then(
+      ([data]) => data,
+    );
+  });
   return (
     <div>
       <div>
@@ -25,11 +41,9 @@ export default function StackDetail({ stackId, goBack }) {
           Return to stacks
         </p>
       </div>
-      {cards.length === 0 ? (
-        <EmptyCard stackId={stackId} setCards={setCards} />
-      ) : (
-        <CardList  stackId={stackId} setCards={setCards} cards={cards} />
-      )}
+      <Suspense fallback={<Loader text={"Loading cards"} />}>
+        <Cards stackId={stackId} getCardsPromise={getCardsPromise} />
+      </Suspense>
     </div>
   );
 }
